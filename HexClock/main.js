0bcfa597f821099b1a736d0f4d21fdd030a8fc51
Charles DeLong <c.thecheesedelong@gmail.com>
2017-03-16 09:50:31 -0700

    Added background image to basic project, and completed hardmode

---
+++ HexClock/main.js
@@ -0,0 +1,22 @@
+
+function repeat(cb, timeframe){
+  setTimeout(function(){
+    cb()
+    repeat(cb, timeframe)
+  }, timeframe)
+}
+
+repeat(function(){
+  var time = new Date() 
+  var h = time.getHours()
+  var m = time.getMinutes()
+  var s = time.getSeconds()
+  h = (h < 10 ? "0" : "") + h
+  h = (h > 12) ? h - 12 : h
+  h = (h == 0) ? 12 : h
+  m = (m < 10 ? "0" : "") + m
+  s = (s < 10 ? "0" : "") + s
+  var hexTime = ("#" + h + m + s)
+  $("#clock").html(hexTime)
+  $("body").css("background-color", hexTime)
+}, 1000)
